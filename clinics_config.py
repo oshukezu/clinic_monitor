1a21d587336b14f7a1650a1fe86298c169e1d887
J.J. Wang <oshukezu@gmail.com>
2026-01-29 16:27:38 +0800

    Add files via upload

---
+++ clinics_config.py
@@ -0,0 +1,74 @@
+# clinics_config.py
+
+# 診所清單設定
+# 請在下方填入 11 家診所的正確資訊
+# 經緯度請使用 Google Maps 右鍵取得 (格式: 24.123456, 120.654321)
+
+CLINICS = [
+    {
+        "name": "高堂中醫",
+        "city": "台中市",
+        "latitude": 24.168379,
+        "longitude": 120.6585075
+    },
+    {
+        "name": "祥順中醫",
+        "city": "彰化市",
+        "latitude": 24.0815,
+        "longitude": 120.5385
+    },
+    {
+        "name": "德善堂中醫",
+        "city": "台北市",
+        "latitude": 25.0435, 
+        "longitude": 121.5583
+    },
+    {
+        "name": "同仁中醫",
+        "city": "新北市",
+        "latitude": 25.0112,
+        "longitude": 121.4623
+    },
+    {
+        "name": "春暉中醫",
+        "city": "高雄市",
+        "latitude": 22.6234,
+        "longitude": 120.3012
+    },
+    {
+        "name": "慈濟中醫",
+        "city": "花蓮市",
+        "latitude": 23.9987,
+        "longitude": 121.5982
+    },
+    {
+        "name": "聖心中醫",
+        "city": "台南市",
+        "latitude": 22.9976,
+        "longitude": 120.2031
+    },
+    {
+        "name": "仁愛中醫",
+        "city": "桃園市",
+        "latitude": 24.9934,
+        "longitude": 121.3121
+    },
+    {
+        "name": "康華中醫",
+        "city": "新竹市",
+        "latitude": 24.8066,
+        "longitude": 120.9688
+    },
+    {
+        "name": "信義中醫",
+        "city": "基隆市",
+        "latitude": 25.1311,
+        "longitude": 121.7456
+    },
+    {
+        "name": "和平中醫",
+        "city": "嘉義市",
+        "latitude": 23.4798,
+        "longitude": 120.4497
+    }
+]
